c417bd667563822f669a3bb3c1be7f6928c8f4d6
Evelyn Fernandes <evelyn.af22@gmail.com>
2023-05-31 20:29:34 -0300

    servico

--- src/app/pages/componentes/home/home.page.ts
+++ src/app/pages/componentes/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AppEmpregetService } from 'src/app/services/app-empreget.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,20 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-  
+  constructor(private AppEmpregetService : AppEmpregetService) { }
+
+/*
+  buscarPrestador(evento){
+    let codigoObjeto = evento.detail.value;
+    this.AppEmpregetService.findAllPrestadorPerfil(codigoObjeto)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(erro => {
+      console.log(erro);
+    });
+  }
+*/  
   ngOnInit() {
   }
 
